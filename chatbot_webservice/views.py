0739357114ae491196041bf1f592f20a566d2b41
Samuel Beck <sammy_13@gmx.net>
2020-09-27 13:33:58 +0200

    Implement simple webhook

--- chatbot_webservice/views.py
+++ chatbot_webservice/views.py
@@ -1,7 +1,8 @@
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
+import json
 import logging
 
 logger = logging.getLogger(__name__)
@@ -10,4 +11,6 @@ logger = logging.getLogger(__name__)
 @csrf_exempt
 def dialogflow(request):
     logger.info('Dialogflow endpoint received a request')
-    return HttpResponse("Hello World. You are looking at the dialogflow endpoint.")
+    req = json.loads(request.body)
+    fulfillmentText = {'fulfillmentText': 'This is a test response sent from the webhook.'}
+    return JsonResponse(fulfillmentText, safe=False)
