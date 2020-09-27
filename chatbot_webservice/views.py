5df4849581a07cb7e2b64a39c63e8fca0cad317b
Samuel Beck <sammy_13@gmx.net>
2020-09-27 13:46:26 +0200

    Implement differentiation of intents

--- chatbot_webservice/views.py
+++ chatbot_webservice/views.py
@@ -12,5 +12,11 @@ logger = logging.getLogger(__name__)
 def dialogflow(request):
     logger.info('Dialogflow endpoint received a request')
     req = json.loads(request.body)
-    fulfillmentText = {'fulfillmentText': 'This is a test response sent from the webhook.'}
+    intent = req.get('queryResult').get('intent').get('displayName')
+
+    if intent == 'Show Architecture':
+        fulfillmentText = {'fulfillmentText': 'Here is your architecture.'}
+    elif intent == 'Show Specification':
+        fulfillmentText = {'fulfillmentText': 'Here is you specification.'}
+
     return JsonResponse(fulfillmentText, safe=False)
