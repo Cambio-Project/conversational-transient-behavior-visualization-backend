144d20808c16f8fc189d0d258464b2e963e01b1b
Samuel Beck <sammy_13@gmx.net>
2020-09-27 13:18:37 +0200

    Add logging

--- chatbot_webservice/views.py
+++ chatbot_webservice/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def dialogflow(request):
+    logger.info('Dialogflow endpoint received a request')
+    return HttpResponse("Hello World. You are looking at the dialogflow endpoint.")
--- webservice/settings.py
+++ webservice/settings.py
@@ -129,3 +129,29 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Activate Django-Heroku
 django_heroku.settings(locals())
+
+# Logging
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} | {module} | {message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '{levelname} {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
