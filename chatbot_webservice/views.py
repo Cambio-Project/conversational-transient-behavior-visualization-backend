3f1ba9c9a1f37ad546e2203df5eea053cec5608a
Samuel Beck <sammy_13@gmx.net>
2020-09-27 13:24:25 +0200

    Add csfr exception for dialogflow view

--- chatbot_webservice/views.py
+++ chatbot_webservice/views.py
@@ -1,3 +1,4 @@
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -6,6 +7,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 
+@csrf_exempt
 def dialogflow(request):
     logger.info('Dialogflow endpoint received a request')
     return HttpResponse("Hello World. You are looking at the dialogflow endpoint.")
