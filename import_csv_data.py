96a3b6e343cd6bd021ffadce6a5c2d0fd2fd9295
Samuel Beck <sammy_13@gmx.net>
2020-10-11 14:46:57 +0200

    Add service data.

--- import_csv_data.py
+++ import_csv_data.py
@@ -1,3 +1,22 @@
 import csv
 
 from chatbot_webservice.models import Service, Dependency, ServiceData
+
+file_path = 'results_monkey_nopattern.csv'
+
+with open(file_path, newline='') as csv_file:
+    data_reader = csv.reader(csv_file, delimiter=',')
+    next(data_reader, None) # skip header
+    for row in data_reader:
+        service_name = row[2]
+        service = Service.objects.get(name=service_name)
+        ServiceData.objects.create(
+            service=service,
+            time=row[0],
+            callId=row[1],
+            uri=row[4],
+            successfulTransactions=row[5],
+            failedTransactions=row[6],
+            droppedTransactions=row[7],
+            avgResponseTime=row[8]
+        )
