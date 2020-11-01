622b375ca7cb9525017976509640b8186aacf551
Samuel Beck <sammy_13@gmx.net>
2020-11-01 23:13:10 +0100

    Add basenames to urls and make migrations

--- chatbot_webservice/urls.py
+++ chatbot_webservice/urls.py
@@ -4,8 +4,8 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r'services', views.ServiceViewSet)
-router.register(r'dependencies', views.DependencyViewSet)
+router.register(r'services', views.ServiceViewSet, basename='service')
+router.register(r'dependencies', views.DependencyViewSet, basename='dependency')
 router.register(r'servicedata', views.ServiceDataViewSet, basename='servicedata')
 router.register(r'specifications', views.SpecificationViewSet, basename='specification')
 
